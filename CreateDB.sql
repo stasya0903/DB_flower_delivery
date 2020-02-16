c607b784811bbd89ff7e0ea0fd09db19946f72c6
stasya0903 <stasya0903@mail.ru>
2020-02-16 14:56:24 -0500

    added the new tables, change the logig a bit

--- CreateDB.sql
+++ CreateDB.sql
@@ -1,4 +1,4 @@
-# Database for the flower delivery company
+#Database for the flower delivery company
 
 DROP DATABASE IF EXISTS flowers_delivery;
 
@@ -6,27 +6,69 @@ CREATE DATABASE flowers_delivery;
 
 USE flowers_delivery;
 
-DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS occasions;
 
+CREATE TABLE occasions(
+id SERIAL PRIMARY KEY,
+name VARCHAR(255)
+);
+
+DROP TABLE IF EXISTS shelf_life_periods;
+
+CREATE TABLE shelf_life_periods(
+id SERIAL PRIMARY KEY,
+name VARCHAR(255),
+amount_of_days BIGINT UNSIGNED NOT NULL
+);
+
+DROP TABLE IF EXISTS categories;
 CREATE TABLE categories(
 id SERIAL PRIMARY KEY,
 name VARCHAR(255)
 );
 
-DROP TABLE IF EXISTS flowers;
 
-CREATE TABLE flowers(
+
+
+DROP TABLE IF EXISTS flowers_and_herbs;
+
+CREATE TABLE flowers_and_herbs(
 id SERIAL PRIMARY KEY,
+category_id BIGINT UNSIGNED NOT NULL,
 name VARCHAR(255),
 color VARCHAR(255),
 description TEXT,
 price DECIMAL(7,2) UNSIGNED NOT NULL,
 country_produced VARCHAR(255),
-image_url VARCHAR(255),
 
-FOREIGN KEY category_for_flowers (category_id) REFERENCES categories(id),
-INDEX category_for_flowers(category_id)
+FOREIGN KEY category_for_item (category_id) REFERENCES categories(id),
 
+INDEX category_for_item (category_id)
+);
+
+
+DROP TABLE IF EXISTS decorations;
+
+CREATE TABLE decorations(
+id SERIAL PRIMARY KEY,
+category_id BIGINT UNSIGNED NOT NULL,
+name VARCHAR(255),
+color VARCHAR(255),
+description TEXT,
+price DECIMAL(7,2) UNSIGNED NOT NULL,
+material VARCHAR(255),
+
+FOREIGN KEY category_for_item (category_id) REFERENCES categories(id),
+INDEX category_for_item (category_id)
+
+);
+
+
+DROP TABLE IF EXISTS bouquets_size;
+
+CREATE TABLE bouquets_size(
+id SERIAL PRIMARY KEY,
+name VARCHAR(255)
 );
 
 DROP TABLE IF EXISTS bouquets;
@@ -37,21 +79,39 @@ name VARCHAR(255),
 description TEXT,
 price DECIMAL(7,2) UNSIGNED NOT NULL,
 image_url VARCHAR(255),
-category_id BIGINT UNSIGNED NOT NULL
+occasion_id BIGINT UNSIGNED NOT NULL,
+size_id BIGINT UNSIGNED NOT NULL,
 
-FOREIGN KEY category_for_flowers (category_id) REFERENCES categories(id),
-INDEX category_for_flowers(category_id)
+FOREIGN KEY occasion_for_bouquete (occasion_id) REFERENCES occasions(id),
+FOREIGN KEY size_of_bouquets(size_id) REFERENCES bouquets_size(id),
+
+INDEX occasion_for_bouquete(occasion_id),
+INDEX size_of_bouquets (size_id)
 
 );
 
-DROP TABLE IF EXISTS flowers_in_bouquets;
 
-CREATE TABLE flowers_in_bouquets(
+DROP TABLE IF EXISTS flowers_and_herbs_in_bouquets;
+
+CREATE TABLE flowers_and_herbs_in_bouquets(
 bouquet_id BIGINT UNSIGNED NOT NULL PRIMARY KEY,
-flower_id BIGINT UNSIGNED NOT NULL,
+item_id BIGINT UNSIGNED NOT NULL,
+quantity_of_used_flowers BIGINT UNSIGNED NOT NULL,
 
 FOREIGN KEY bouquet_from (bouquet_id) REFERENCES bouquets(id),
-FOREIGN KEY flower_in(flower_id) REFERENCES flowers(id),
+FOREIGN KEY item_in(item_id) REFERENCES flowers_and_herbs(id)
+
+);
+
+DROP TABLE IF EXISTS decorations_in_bouquets;
+
+CREATE TABLE decorations_in_bouquets(
+bouquet_id BIGINT UNSIGNED NOT NULL PRIMARY KEY,
+item_id BIGINT UNSIGNED NOT NULL,
+quantity_of_used_decoration BIGINT UNSIGNED NOT NULL,
+
+FOREIGN KEY bouquet_from (bouquet_id) REFERENCES bouquets(id),
+FOREIGN KEY item_in(item_id) REFERENCES decorations(id)
 
 );
 
@@ -59,13 +119,15 @@ DROP TABLE IF EXISTS discounts;
 
 CREATE TABLE discount(
 id SERIAL PRIMARY KEY,
-starting_at DATETIME,
-finishing_at DATETIME,
+starting_on DATETIME,
+finishing_on DATETIME,
 amount BIGINT UNSIGNED NOT NULL,
-catergory_id BIGINT UNSIGNED NOT NULL,
+made_for_item_id BIGINT UNSIGNED NOT NULL,
 
-FOREIGN KEY discounts_by_categories (catergory_id) REFERENCES categories(id),
-INDEX discounts_by_categories (catergory_id)
+FOREIGN KEY discounts_by_occasion(made_for_item_id) REFERENCES occasions(id),
+FOREIGN KEY bouquets_by_bouquets (made_for_item_id) REFERENCES bouquets(id),
+
+INDEX discounts_by (made_for_item_id)
 );
 
 DROP TABLE IF EXISTS users;
@@ -110,21 +172,65 @@ id SERIAL PRIMARY KEY,
 sender BIGINT UNSIGNED NOT NULL,
 item_id BIGINT UNSIGNED NOT NULL,
 destination_id BIGINT UNSIGNED NOT NULL,
+discount_id BIGINT UNSIGNED NOT NULL,
 created DATETIME,
 deliver_at DATETIME,
 `status` ENUM('pending payment', 'paid', 'preparing', 'outForDelivery', 'delivered', 'canceled'),
 message TEXT,
 
-FOREIGN KEY sender (user_id) REFERENCES users(id),
+FOREIGN KEY sender_key (sender) REFERENCES users(id),
 FOREIGN KEY item_to_delivery (item_id) REFERENCES bouquets(id),
 FOREIGN KEY deliver_to (destination_id) REFERENCES delivery_destinations(id),
+FOREIGN KEY discount_for_order (discount_id) REFERENCES discount(id),
 
-INDEX sender (user_id),
+INDEX sender_indx (sender),
 INDEX item_to_delivery (item_id),
 INDEX deliver_to (destination_id)
 
 
 );
+
+DROP TABLE IF EXISTS supplies;
+CREATE TABLE supplies (
+  id SERIAL PRIMARY KEY,
+  item_id BIGINT UNSIGNED NOT NULL,
+  category_id BIGINT UNSIGNED NOT NULL,
+  value INT UNSIGNED,
+  supplied_on DATETIME DEFAULT CURRENT_TIMESTAMP,
+  shelf_life_period_id BIGINT UNSIGNED NOT NULL,
+  expiration_date DATETIME NOT NULL, # должен расчитываться???
+   
+  FOREIGN KEY item_id_key(item_id) REFERENCES flowers_and_herbs(id),
+  FOREIGN KEY item_id_key(item_id) REFERENCES decorations(id),
+  FOREIGN KEY category_of_item(category_id) REFERENCES categories (id),
+  FOREIGN KEY shelf_life_period_id_of_item(shelf_life_period_id) REFERENCES shelf_life_periods(id),
+  
+  INDEX item_id_indx (item_id),
+  INDEX category_of_item(category_id)
+);
+
+
+DROP TABLE IF EXISTS inventory;
+CREATE TABLE inventory (
+  item_id BIGINT UNSIGNED NOT NULL,
+  category_id BIGINT UNSIGNED NOT NULL,
+  supply_id BIGINT UNSIGNED NOT NULL,
+  value INT UNSIGNED,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+   
+  PRIMARY KEY(item_id, category_id),
+  FOREIGN KEY item_id_key(item_id) REFERENCES flowers_and_herbs(id),
+  FOREIGN KEY item_id_key(item_id) REFERENCES decorations(id),
+  FOREIGN KEY sypply_info(supply_id) REFERENCES supplies (id),
+  
+  
+  INDEX item_id_indx (item_id)
+);
+
+
+
+
+
 
 
 
